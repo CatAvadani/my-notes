de79fa99ad0ffb902261d3ca718389469cbe6aa0
CatAvadani <catalinaavadani@gmail.com>
2024-03-31 18:47:48 +0200

    Add an edit icon.

--- app/allNotes/page.tsx
+++ app/allNotes/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
 import { XMarkIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import { useNotes } from "../contexts/NoteContext";
@@ -9,7 +10,7 @@ export default function AllNotes() {
 
   return (
     <div className=' flex flex-col mt-24  text-gray-700 justify-center items-center'>
-      <h1 className='text-2xl md:text-3xl font-bold mb-10 mt-0'>YOUR NOTES</h1>
+      <h1 className='text-2xl md:text-3xl font-bold mb-10 mt-0'>MY NOTES</h1>
       <div className='flex flex-1 w-full'>
         <Link href={"/"}>
           <button
@@ -28,10 +29,13 @@ export default function AllNotes() {
           >
             <div className='flex justify-between'>
               <h1 className=' font-semibold mb-2 capitalize'>{note.title}</h1>
-              <XMarkIcon
-                onClick={() => removeNote(note)}
-                className='h-6 w-6 text-red-700 cursor-pointer hover:scale-125 transition-all'
-              />
+              <div className='flex justify-center gap-2'>
+                <PencilSquareIcon className=' size-5 text-gray-600 cursor-pointer hover:scale-125 transition-all' />
+                <XMarkIcon
+                  onClick={() => removeNote(note)}
+                  className=' size-6 text-red-700 cursor-pointer hover:scale-125 transition-all'
+                />
+              </div>
             </div>
             <hr />
             <p className='mt-4'>{note.text}</p>

b7be4b604dc5882b35a7ff5e82bd300180d932a9
CatAvadani <catalinaavadani@gmail.com>
2024-03-31 16:52:46 +0200

    Save notes to the local storage.

--- app/contexts/NoteContext.tsx
+++ app/contexts/NoteContext.tsx
@@ -1,5 +1,11 @@
 "use client";
-import { PropsWithChildren, createContext, useContext, useState } from "react";
+import {
+  PropsWithChildren,
+  createContext,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
 export interface Note {
   title: string;
@@ -15,10 +21,26 @@ const NoteContext = createContext({} as NoteContextValue);
 
 function NotesProvider(props: PropsWithChildren) {
   const [notes, setNotes] = useState<Note[]>([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    const savedNotes = localStorage.getItem("notes");
+    if (savedNotes) {
+      setNotes(JSON.parse(savedNotes));
+    }
+    setIsLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes, isLoaded]);
 
   const addNotes = (note: Note) => {
     setNotes([...notes, note]);
   };
+
+  console.log(notes);
 
   return (
     <NoteContext.Provider
